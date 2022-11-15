d30ef24fe1a25a96908a9a4988eb34474c000d14
OGMaksiu <mateckimaksymilian@gmail.com>
2022-11-15 13:51:16 +0100

    dodano zmienne

--- app/src/main/java/com/barstool/project2/MainActivity.java
+++ app/src/main/java/com/barstool/project2/MainActivity.java
@@ -3,12 +3,26 @@ package com.barstool.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
+    Button btn;
+    Switch onOff;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.imageView);
+        btn = findViewById(R.id.butName);
+        onOff = findViewById(R.id.onOff);
+        txt = findViewById(R.id.lokalizacjaText);
     }
 }
